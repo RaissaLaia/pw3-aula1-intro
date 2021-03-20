e261fffe2af2bbd28075ffb2110d2c716a7687a5
RaissaLaia <benini.raissa12@gmail.com>
2021-03-20 00:00:04 +0000

    Aula 01 - Primeiro Componente

---
+++ fundamentos1/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import HeyWorld from './components/basics/HeyWorld'
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1> Aula 01 - Fundamentos 01 :D </h1>
+      
+            <HeyWorld />
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ fundamentos1/src/components/basics/HeyWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => {
+    const informacao = 'Primeiro componente jsx'
+
+    return(
+        <div>
+            <h2> Fala galerinha habitante do planetinha azul </h2>
+            <p>{ informacao}</p>
+        </div>
+    )
+}
